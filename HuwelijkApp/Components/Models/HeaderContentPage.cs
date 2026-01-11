ad4a536d5504a63135f7b4ed9f278360d6a30311
Roeland Krijgsman <roelandkrijgsman@gmail.com>
2026-01-11 17:06:48 +0100

    Initial but generated from Rider instead of copilot + setup pages

---
+++ HuwelijkApp/Components/Models/HeaderContentPage.cs
@@ -0,0 +1,8 @@
+﻿namespace HuwelijkApp.Components.Models;
+
+public class HeaderContentPage(string link, string text, string icon = null)
+{
+    public string Link = link;
+    public string Text = text;
+    public string Icon = icon;
+}
---
+++ HuwelijkApp/Program.cs
@@ -0,0 +1,26 @@
+using HuwelijkApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
