c8bc07304214f9ea168294772f2a83d42143c7b3
Roeland Krijgsman <roelandkrijgsman@gmail.com>
2026-01-25 17:43:01 +0100

    Read Honeymoon parts from configuration

--- HuwelijkApp/Components/Models/Donation.cs
+++ HuwelijkApp/Components/Models/Donation.cs
@@ -1,7 +1,15 @@
 ﻿namespace HuwelijkApp.Components.Models;
 
-public class Donation(string header, string text, string id) {
-    public string Header = header;
-    public string Text = text;
-    public string Id = id;
+public class Donation {
+    public Donation() { }
+
+    public Donation(string header, string text, string id) {
+        Header = header;
+        Text = text;
+        Id = id;
+    }
+
+    public string Header { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
 }
