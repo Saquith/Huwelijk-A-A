caf65e6e1f7ba05fe3d130b28e4ede74084cdd6d
Roeland Krijgsman <roelandkrijgsman@gmail.com>
2026-01-26 21:56:51 +0100

    PR comments, again

--- HuwelijkApp/Components/Models/Donation.cs
+++ HuwelijkApp/Components/Models/Donation.cs
@@ -3,12 +3,14 @@
 public class Donation {
     public Donation() { }
 
-    public Donation(string header, string text, string id) {
+    public Donation(string header, string text, string id, string link) {
         Header = header;
         Text = text;
         Id = id;
+        Link = link;
     }
 
+    public string Link { get; set; } = string.Empty;
     public string Header { get; set; } = string.Empty;
     public string Text { get; set; } = string.Empty;
     public string Id { get; set; } = string.Empty;
